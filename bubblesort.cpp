fef73b49aba0032df87fab6fef049df31b197189
ttoolls <a_selivanov@mail.ru>
2020-01-20 18:56:30 +0300

    first attempt to implement bubblesort

---
+++ bubblesort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "bubblesort.h"
+
+void bubbleSort(int* ar, int size)
+{
+	if (size < 0)
+	{
+		std::cout << "Incorrect size of array" << std::endl;
+		return;
+	}
+	for (int i = 0; i < size - 1; i++)
+	{
+		bool flag = false;
+		for (int j = 0; j < size - i - 1; j++)
+		{
+			if (ar[j] > ar[j + 1])
+			{
+				int Tmp = ar[j];
+				ar[j] = ar[j + 1];
+				ar[j + 1] = Tmp;
+				flag = true;
+			}
+		}
+		if (flag == false)
+			break;
+	}
+}
---
+++ bubblesort.h
@@ -0,0 +1,2 @@
+#pragma once
+void bubbleSort(int* ar, int n);
